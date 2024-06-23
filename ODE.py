a60ab471b7a94b7cf6ed0443654527d68635f02b
admarrero <a.marrero@estudiantec.cr>
2024-06-22 19:31:11 -0600

    comment on ODE

--- ODE.py
+++ ODE.py
@@ -1,3 +1,5 @@
+#ODE/ODE.py
+
 #importar bibliotecas y definir la función a resolver
 
 import numpy as np
@@ -5,10 +7,41 @@ import scipy as sp
 import matplotlib.pyplot as plt
 
 def func(x,t):
+    """Function of the derivative 
+
+    Examples:
+        >>> func(2.0,3.0)
+        -7.86
+
+    Args:
+        x (float): First argument
+        t (float): Second argument
+
+    Returns:
+        float: Returns the derivative evaluate in 'x' and 't'
+
+    """
     return -x**3 + np.sin(t)
 
 #método de Euler
 def euler(f, x0, times):
+    """Euler method
+
+    Examples:
+        >>> euler(func,0.0,np.linspace(0.0,10.0,20.0)
+        [ 0.          0.          0.26439534  0.71189381  1.04830651  0.89488942
+        0.77464602  0.52141013  0.17502349 -0.28921312 -0.80263945 -0.97897518
+        -0.73458315 -0.50879969 -0.16038526  0.30726691  0.81787725  0.97386719
+        0.72957635  0.49945695]
+
+    Args:
+        f (function): First argument, derivative function
+        x0 (float) : Second argument, initial condition
+        times (array): Third argument, array of steps
+
+    Returns:
+        array: Returns the array of approximations from x0 to the size of times minus one
+    """
     h=times[1]-times[0]
     xn=np.zeros(times.size)
     xn[0]=x0
@@ -16,28 +49,26 @@ def euler(f, x0, times):
         xn[i+1]=xn[i]+(h*f(xn[i],times[i]))
     return xn
 
-#definición de las variables
-x_0 = 0
-x0=np.copy(x_0)
-t_0 = 0
-
-N1=20
-N2=100
-
-#Ejecución
-y1_eu=euler(func,x_0,np.linspace(t_0, 10.0, N1))
-y2_eu=euler(func,x_0,np.linspace(t_0, 10.0, N2))
-
-
-#gráfico
-plt.plot(np.linspace(t_0, 10.0, N1), y1_eu, 'r')
-plt.plot(np.linspace(t_0, 10.0, N2), y2_eu, 'b')
-
-plt.show()
-
 
 #RK2
 def rk2(f, x0, times):
+    """ Runge-Kutta 2 method
+
+    Examples:
+        >>> rk2(func,0.0,np.linspace(0.0,10.0,20.0)
+        [ 0.          0.13691106  0.50040599  0.83221858  0.89696773  0.83638552
+        0.684369    0.42791827  0.0377284  -0.46988042 -0.7896376  -0.78706438
+        -0.65422559 -0.40234264 -0.0089812   0.49847514  0.79691697  0.78634182
+        0.64914835  0.39298319]
+
+    Args:
+        f (function): First argument, derivative function
+        x0 (float): Second argument, initial condition
+        times (array): Third argument, array of steps
+
+    Returns:
+        array: Returns the array of approximations from x0 to the size of times minus one
+    """
     h = times[1] - times[0]
     xn=np.zeros(times.size)
     xn[0]=x0
@@ -48,18 +79,27 @@ def rk2(f, x0, times):
         xn[i+1]=xn[i]+k2
     return xn
 
-#Ejecución
-y1_rk2=rk2(func,0,np.linspace(0, 10.0, N1))
-y2_rk2=rk2(func,0,np.linspace(0, 10.0, N2))
 
-#Gráfica
-plt.plot(np.linspace(t_0, 10.0, N1), y1_rk2, 'r')
-plt.plot(np.linspace(t_0, 10.0, N2), y2_rk2, 'b')
-
-plt.show()
 
 #RK4
 def rk4(f, x0, times):
+    """Runge-Kutta 4 method
+
+    Examples:
+        >>> rk4(func,0.0,np,linspace(0.0,10.0,20.0)
+        [ 0.          0.13505937  0.48487586  0.81979626  0.93102462  0.88144889
+        0.72370945  0.46026479  0.06848691 -0.42773679 -0.78084333 -0.83096351
+        -0.6993985  -0.43991777 -0.04506765  0.45089026  0.79057875  0.83047637
+        0.69376603  0.43014634]
+
+    Args:
+        f (function): First argument, derivative function
+        x0 (float): Second argument, initial condition
+        times (array): Third argument, array of steps
+
+    Returns:
+        array: Returns the array of approximations from x0 to the size of times minus one
+    """
     h = times[1] - times[0]
     xn=np.zeros(times.size)
     xn[0]=x0
@@ -71,14 +111,3 @@ def rk4(f, x0, times):
         k4=h*f(xn[i]+k3,times[i]+h)
         xn[i+1]=xn[i]+(k1+2*k2+2*k3+k4)/6
     return xn
-
-#Ejecución
-y1_rk4=rk4(func,0,np.linspace(0, 10.0, N1))
-y2_rk4=rk4(func,0,np.linspace(0, 10.0, N2))
-
-#Gráfico
-plt.plot(np.linspace(t_0, 10.0, N1), y1_rk4, 'r')
-plt.plot(np.linspace(t_0, 10.0, N2), y2_rk4, 'b')
-
-plt.show()
-
